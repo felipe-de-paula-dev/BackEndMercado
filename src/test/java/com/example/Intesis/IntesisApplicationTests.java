8354c6f054de99b246becbd9ce0816de2f6bc808
Felipe De Paula <noxus19blue12@gmail.com>
2025-04-29 00:52:19 -0300

    Subindo Aplicação Completa  BackEnd

---
+++ src/test/java/com/example/Intesis/IntesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Intesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
